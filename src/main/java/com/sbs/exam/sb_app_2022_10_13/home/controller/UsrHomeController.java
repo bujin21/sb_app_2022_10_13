d7f311b04700a7cdfaae07d9b7a8d8710dab2f4e
bujin21 <cvj0810@naver.com>
2022-11-20 17:46:27 +0900

    게시물 컨트롤러에, 게시물 추가, 리스트 기능 구현

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/home/controller/UsrHomeController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/home/controller/UsrHomeController.java
@@ -1,8 +1,6 @@
 package com.sbs.exam.sb_app_2022_10_13.home.controller;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.sbs.exam.sb_app_2022_10_13.article.vo.Article;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -85,7 +83,7 @@ public class UsrHomeController {
   @RequestMapping("/usr/home/getArticle")
   @ResponseBody
   public Article getArticle() {
-    Article article = new Article(1, "제목1");
+    Article article = new Article(1, "제목1","내용1");
 
     return article;
   }
@@ -93,8 +91,8 @@ public class UsrHomeController {
   @RequestMapping("/usr/home/getArticles")
   @ResponseBody
   public List<Article> getArticles() {
-    Article article1 = new Article(1, "제목1");
-    Article article2 = new Article(2, "제목2");
+    Article article1 = new Article(1, "제목1","내용1");
+    Article article2 = new Article(2, "제목2","내용1");
 
     List<Article> list = new ArrayList<>();
     list.add(article1);
@@ -108,11 +106,4 @@ public class UsrHomeController {
    this.count=count;
     return "count의 값이 " + this.count + "으로 초기화 되었습니다.";
   }
-}
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-class Article{
-  public int id;
-  public String title;
 }
