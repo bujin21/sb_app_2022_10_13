6fcea470e26a181f989214d45f89051dccd67c76
bujin21 <cvj0810@naver.com>
2022-11-21 19:18:00 +0900

    getArticle, deleteArticle 서비스 메서드로 게시물 삭제기능 구현

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/article/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/article/controller/UsrArticleController.java
@@ -10,10 +10,12 @@ import java.util.List;
 
 @Controller
 public class UsrArticleController {
-
+  // 인스턴스 변수 시작
   int articleLastId;
   private List<Article> articles;
+  // 인스턴스 변수 끝
 
+  // 생성자
   public UsrArticleController() {
     articles = new ArrayList<>();
     articleLastId  = 0;
@@ -21,6 +23,7 @@ public class UsrArticleController {
     makeTestData();
   }
 
+  // 서비스 메서드 시작
   private void makeTestData() {
     for(int i = 1; i <= 10; i++){
       String title = "제목" + i;
@@ -30,9 +33,18 @@ public class UsrArticleController {
     }
   }
 
+  private Article getArticle(int id) {
+    for ( Article article : articles ) {
+      if ( article.getId() == id ) {
+        return article;
+      }
+    }
+    return null;
+  }
+
   private Article writeArticle(String title, String body) {
     int id = articleLastId + 1;
-    Article article = new Article(id,title, body);
+    Article article = new Article(id, title, body);
 
     articles.add(article);
     articleLastId = id;
@@ -40,6 +52,14 @@ public class UsrArticleController {
     return article;
   }
 
+  private void deleteArticle(int id) {
+    Article article = getArticle(id);
+
+    articles.remove(article);
+  }
+  // 서비스 메서드 끝
+
+  // 액션 메서드 시작
   @RequestMapping("/usr/article/doAdd")
   @ResponseBody
   public Article doAdd(String title, String body) {
@@ -53,4 +73,20 @@ public class UsrArticleController {
   public List<Article> getArticles() {
     return articles;
   }
+
+  @RequestMapping("/usr/article/doDelete")
+  @ResponseBody
+  public String dolDelete(int id) {
+    Article article = getArticle(id);
+
+    if(article == null){
+      return id + "번 게시물이 존재하지 않습니다.";
+    }
+
+    deleteArticle(id);
+
+    return id + "번 게시물이 삭제되었습니다.";
+  }
+  // 액션 메서드 끝
+
 }
