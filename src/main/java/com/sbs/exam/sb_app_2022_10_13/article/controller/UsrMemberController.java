2964605366c092f85b2f13281bfbe5156e6d0502
bujin21 <cvj0810@naver.com>
2022-11-23 21:33:28 +0900

    회원 가입 시, 입력데이터 유효성 체크, 공백도 체크

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/article/controller/UsrMemberController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/article/controller/UsrMemberController.java
@@ -23,33 +23,35 @@ public class UsrMemberController {
 
     Member member = memberService.getMemberById(id);
 
+
+    if( loginId == null || loginId.trim().length() == 0) {
+      return "loginId(을)를 입력 해주세요.";
+    }
+
+    if( loginPw == null || loginPw.trim().length() == 0) {
+      return "loginPw(을)를 입력 해주세요.";
+    }
+
+    if( name == null || name.trim().length() == 0) {
+      return "name(을)를 입력 해주세요.";
+    }
+
+    if( nickname == null || nickname.trim().length() == 0) {
+      return "nickname(을)를 입력 해주세요.";
+    }
+
+    if( cellphoneNo == null || cellphoneNo.trim().length() == 0) {
+      return "cellphoneNo(을)를 입력 해주세요.";
+    }
+
+    if( email == null || email.trim().length() == 0) {
+      return "email(을)를 입력 해주세요.";
+    }
+
     if ( id == -1 ) {
       return "해당 로그인아이디는 이미 사용중입니다.";
     }
 
-    if( loginId == null ) {
-      return "loginId(을)를 입력 해주세요.";
-    }
-
-    if( loginPw == null ) {
-      return "loginPw(을)를 입력 해주세요.";
-    }
-
-    if( name == null ) {
-      return "name(을)를 입력 해주세요.";
-    }
-
-    if( nickname == null ) {
-      return "nickname(을)를 입력 해주세요.";
-    }
-
-    if( cellphoneNo == null ) {
-      return "cellphoneNo(을)를 입력 해주세요.";
-    }
-
-    if( email == null ) {
-      return "email(을)를 입력 해주세요.";
-    }
     return member;
   }
 
