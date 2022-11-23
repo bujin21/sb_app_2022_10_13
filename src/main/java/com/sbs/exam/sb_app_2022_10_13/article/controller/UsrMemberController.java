ce6c11e3d34690a8a012c10852ea3c1c9644d917
bujin21 <cvj0810@naver.com>
2022-11-23 20:21:22 +0900

    회원 가입 완료 후, 생성된 회원 보여줌

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/article/controller/UsrMemberController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/article/controller/UsrMemberController.java
@@ -1,6 +1,7 @@
 package com.sbs.exam.sb_app_2022_10_13.article.controller;
 
 import com.sbs.exam.sb_app_2022_10_13.article.service.MemberService;
+import com.sbs.exam.sb_app_2022_10_13.article.vo.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,11 +17,13 @@ public class UsrMemberController {
 
   @RequestMapping("/usr/member/doJoin")
   @ResponseBody
-  public String doJoin(String loginId, String loginPw, String name, String nickname,
-                        String cellphoneNo, String email) {
-    memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+  public Member doJoin(String loginId, String loginPw, String name, String nickname,
+                       String cellphoneNo, String email) {
+    int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 
-    return "성공";
+    Member member = memberService.getMemberById(id);
+
+    return member;
   }
 
 }
--- src/main/java/com/sbs/exam/sb_app_2022_10_13/article/repository/MemberRepository.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/article/repository/MemberRepository.java
@@ -1,8 +1,10 @@
 package com.sbs.exam.sb_app_2022_10_13.article.repository;
 
+import com.sbs.exam.sb_app_2022_10_13.article.vo.Member;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface MemberRepository {
@@ -19,5 +21,17 @@ public interface MemberRepository {
           """)
   public void join(@Param("loginId") String loginId, @Param("loginPw") String loginPw, @Param("name") String name,
                    @Param("nickname") String nickname, @Param("cellphoneNo") String cellphoneNo, @Param("email") String email);
+
+  @Select("""
+          SELECT LAST_INSERT_ID()
+          """)
+  public int getLastInsertId();
+
+  @Select("""
+          SELECT *
+          FROM `member` AS M
+          WHERE M.id = #{id}
+          """)
+  Member getMemberById(int id);
 }
 
