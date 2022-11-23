2732404a921b1bc55526bafd8ea203717e99b34b
bujin21 <cvj0810@naver.com>
2022-11-23 19:06:07 +0900

    게시물 수정시, 입력된 정보만 수정

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/article/repository/ArticleRepository.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/article/repository/ArticleRepository.java
@@ -8,19 +8,51 @@ import java.util.List;
 @Mapper
 public interface ArticleRepository {
 
-  @Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body` = #{body}")
+  @Insert("""
+           INSERT INTO article
+           SET regDate = NOW(),
+           updateDate = NOW(),
+           title = #{title},
+           `body` = #{body}
+          """)
   public void writeArticle(String title, String body);
 
-  @Select("SELECT * FROM article WHERE id = #{id}")
+  @Select("""
+          SELECT *
+          FROM article
+          WHERE id = #{id}
+          """)
   public Article getArticle(int id);
 
-  @Delete("DELETE FROM article WHERE id = #{id}")
+  @Delete("""
+          DELETE
+          FROM article
+          WHERE id = #{id}
+          """)
   public void deleteArticle(int id) ;
 
-  @Select("SELECT * FROM article ORDER BY id DESC")
+  @Select("""
+          SELECT *
+          FROM article
+          ORDER BY id DESC
+          """)
   public List<Article> getArticles();
 
-  @Update("UPDATE article SET title = #{title}, `body` = #{body}, updateDate = NOW() WHERE id = #{id}")
+  @Update("""
+          <script>
+          UPDATE article
+          <set>
+            <if test='title != null'>
+              title = #{title},
+            </if>
+            <if test='body != null'>
+              `body` = #{body},
+            </if>
+            updateDate = NOW()
+          </set>
+          WHERE id = #{id}
+          </script>
+          """)
   public void modifyArticle(int id, String title, String body);
 
   @Select("SELECT LAST_INSERT_ID()")
