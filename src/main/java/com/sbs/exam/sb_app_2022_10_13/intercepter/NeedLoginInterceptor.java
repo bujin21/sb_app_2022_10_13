0e5ba5c8b8fae7e1ddacf1378156aa79fd5b6ea0
bujin21 <cvj0810@naver.com>
2022-12-05 19:19:59 +0900

    NeedLogin인터셉터가 여러 액션들을 대신해서 자격없는 요청들을 앞선에서 거름

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/intercepter/NeedLoginInterceptor.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/intercepter/NeedLoginInterceptor.java
@@ -1,5 +1,6 @@
 package com.sbs.exam.sb_app_2022_10_13.intercepter;
 
+import com.sbs.exam.sb_app_2022_10_13.vo.Rq;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -10,7 +11,12 @@ import javax.servlet.http.HttpServletResponse;
 public class NeedLoginInterceptor implements HandlerInterceptor {
   @Override
   public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handle) throws Exception {
-    System.out.println("로그인 필요");
+    Rq rq = (Rq) req.getAttribute("rq");
+
+    if(!rq.isLogined()){
+      rq.printHistoryBackJs("로그인 후 이용해주세요.");
+      return false;
+    }
 
     return HandlerInterceptor.super.preHandle(req, resp, handle);
   }
--- src/main/java/com/sbs/exam/sb_app_2022_10_13/vo/Rq.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/vo/Rq.java
@@ -1,16 +1,25 @@
 package com.sbs.exam.sb_app_2022_10_13.vo;
 
+import com.sbs.exam.sb_app_2022_10_13.util.Ut;
 import lombok.Getter;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 public class Rq {
   @Getter
   private boolean isLogined;
   @Getter
   private int loginedMemberId;
-  public Rq(HttpServletRequest req) {
+
+  private HttpServletRequest req;
+  private HttpServletResponse resp;
+  public Rq(HttpServletRequest req, HttpServletResponse resp) {
+    this.req = req;
+    this.resp = resp;
+
     HttpSession httpSession = req.getSession();
     boolean isLogined = false;
     int loginedMemberId = 0;
@@ -25,4 +34,29 @@ public class Rq {
   }
 
 
+  public void printHistoryBackJs(String msg) {
+    resp.setContentType("text/html; charset=UTF-8");
+
+    println("<script>");
+
+    if(!Ut.emty(msg)){
+      println("alert('" + msg + "');");
+    }
+
+    println("history.back();");
+
+    println("</script>");
+  }
+
+  private void println(String str) {
+    print(str + "\n");
+  }
+
+  private void print(String str) {
+    try{
+      resp.getWriter().append(str);
+    }catch (IOException e){
+      throw new RuntimeException(e);
+    }
+  }
 }
