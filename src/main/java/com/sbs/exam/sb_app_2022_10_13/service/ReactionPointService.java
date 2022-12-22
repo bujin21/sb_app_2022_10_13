91da1f315532744c8f563be123d3b05004b54bdd
bujin21 <cvj0810@naver.com>
2022-12-22 21:58:47 +0900

    좋아요 취소버튼, 싫어요 취소버튼을 조건에 따라 노출

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/service/ReactionPointService.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/service/ReactionPointService.java
@@ -14,12 +14,18 @@ public class ReactionPointService {
     this.articleService = articleService;
   }
 
-  public boolean actorCanMakeReactionPoint(int actorId, String relTypeCode, int id) {
+  public ResultData actorCanMakeReactionPoint(int actorId, String relTypeCode, int relId) {
     if(actorId == 0){
-      return false;
+      return ResultData.from("F-1", "로그인 후 이용해주세요.");
     }
 
-    return reactionPointRepository.getSumReactionPointByMemberId(relTypeCode, actorId, id) == 0;
+    int sumReactionPointByMemberId = reactionPointRepository.getSumReactionPointByMemberId(relTypeCode, relId, actorId);
+
+    if (sumReactionPointByMemberId != 0) {
+      return ResultData.from("F-2", "리액션이 불가능합니다.", "sumReactionPointByMemberId", sumReactionPointByMemberId);
+    }
+
+    return ResultData.from("S-1", "리액션이 가능합니다.", "sumReactionPointByMemberId", sumReactionPointByMemberId);
   }
 
   public ResultData addGoodReactionPoint(int actorId, String relTypeCode, int relId) {
@@ -42,7 +48,6 @@ public class ReactionPointService {
         articleService.increaseBadReactionPoint(relId);
         break;
     }
-
     return ResultData.from("S-1", "싫어요 처리 되었습니다.");
   }
 }
