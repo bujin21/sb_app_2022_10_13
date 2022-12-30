28d1265b35eff93be54ecce3bd2903ed014d6505
bujin21 <cvj0810@naver.com>
2022-12-30 19:19:30 +0900

    댓글 삭제

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/repository/ReplyRepository.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/repository/ReplyRepository.java
@@ -1,10 +1,7 @@
 package com.sbs.exam.sb_app_2022_10_13.repository;
 
 import com.sbs.exam.sb_app_2022_10_13.vo.Reply;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -20,7 +17,7 @@ public interface ReplyRepository {
           relId = #{relId},
           body = #{body}
           """)
-  public void writeArticle(@Param("memberId") int memberId,@Param("relTypeCode") String relTypeCode, @Param("relId") int relId,@Param("body") String body);
+  public void writeReply(@Param("memberId") int memberId,@Param("relTypeCode") String relTypeCode, @Param("relId") int relId,@Param("body") String body);
 
   @Select("""
           SELECT LAST_INSERT_ID()
@@ -28,7 +25,8 @@ public interface ReplyRepository {
   int getLastInsertId();
 
   @Select("""
-          SELECT R.*
+          SELECT R.*,
+          M.nickname AS extra__writerName
           FROM reply AS R
           LEFT JOIN `member` AS M 
           ON R.memberId = M.id
@@ -37,4 +35,26 @@ public interface ReplyRepository {
           ORDER BY R.id DESC
           """)
   List<Reply> getForPrintReplies(@Param("relTypeCode") String relTypeCode, @Param("relId")int relId);
+
+  @Select("""
+          SELECT R.*,
+          M.nickname AS extra__writerName
+          FROM reply AS R
+          LEFT JOIN `member` AS M
+          ON R.memberID = M.id
+          WHERE R.id = #{id}
+          """)
+  Reply getForPrintReply(@Param("id") int id);
+  @Select("""
+          SELECT R.*
+          FROM reply AS R
+          WHERE R.id = #{id}
+          """)
+  Reply getReply(@Param("id") int id);
+
+  @Delete("""
+          DELETE FROM reply
+          WHERE id = #{id}
+          """)
+  void deleteReply(@Param("id") int id);
 }
